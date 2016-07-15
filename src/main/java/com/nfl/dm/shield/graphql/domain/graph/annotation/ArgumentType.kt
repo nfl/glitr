e06855a3a0beb85154c93a4a0a8ff3cd39b7e9cc
Boyer, Antoine <Antoine.Boyer@NFL.com>
2016-07-15 16:06:46 -0700

    Added QUERY_EXISTS to ArgumentType
    Test Plan: ./gradlew test
    
    Reviewers: #shield, ashokoufandeh
    
    Reviewed By: #shield, ashokoufandeh
    
    Subscribers: jenkins_github_api
    
    Differential Revision: https://phabricator.dm.nfl.com/D18871

--- src/main/java/com/nfl/dm/shield/graphql/domain/graph/annotation/ArgumentType.kt
+++ src/main/java/com/nfl/dm/shield/graphql/domain/graph/annotation/ArgumentType.kt
@@ -3,6 +3,7 @@ package com.nfl.dm.shield.graphql.domain.graph.annotation
 enum class ArgumentType private constructor(postFix: String) {
     QUERY(""),
     QUERY_CONTAINS("CONTAINS"),
+    QUERY_EXISTS("EXISTS"),
     QUERY_CUSTOM(""),
     ORDER_BY(""),
     ORDER_DIRECTION(""),

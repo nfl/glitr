e1cd4aa38257439438d34258515734b4caf9cdd6
Boyer, Antoine <Antoine.Boyer@NFL.com>
2016-07-15 16:58:06 -0700

    Remove zeppelin specific annotation stuff on @Argument
    Test Plan: ./gradlew test
    
    Reviewers: #shield, ckim
    
    Reviewed By: #shield, ckim
    
    Subscribers: jenkins_github_api
    
    Differential Revision: https://phabricator.dm.nfl.com/D18905

--- src/main/java/com/nfl/dm/shield/graphql/domain/graph/annotation/Argument.kt
+++ src/main/java/com/nfl/dm/shield/graphql/domain/graph/annotation/Argument.kt
@@ -12,4 +12,9 @@ import kotlin.reflect.KClass
 @Retention
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Repeatable
-annotation class Argument(val name: String, val type: KClass<*>, val argumentType: ArgumentType = ArgumentType.QUERY, val customQueryFragmentProvider: String = "", val nullable: Boolean = true, val defaultValue: String = "No Default Value", val description: String = "No Description")
+annotation class Argument(
+        val name: String,
+        val type: KClass<*>,
+        val nullable: Boolean = true,
+        val defaultValue: String = "No Default Value",
+        val description: String = "No Description")

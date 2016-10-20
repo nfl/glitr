49501e90782d9e2a398a39fd79e634adaaf10e9c
Boyer, Antoine <Antoine.Boyer@NFL.com>
2016-10-20 15:45:56 -0700

    Run the command. duh

--- travis-build.sh
+++ travis-build.sh
@@ -5,3 +5,4 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
     echo "Building on master"
     BUILD_COMMAND="./gradlew assemble && ./gradlew check && ./gradlew bintrayUpload -x check --info"
 fi
+bash -c "${BUILD_COMMAND}"

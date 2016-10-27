7b2205b435f741bae320f5112e3bb2f1603c1a09
Boyer, Antoine <Antoine.Boyer@NFL.com>
2016-10-27 15:06:36 -0700

    Enable bintrayUpload via Travis for non-pull requests

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,6 @@ set -ev
 BUILD_COMMAND="./gradlew assemble && ./gradlew check"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "Building on master"
-    # Below is disabled until we get repo gets added into jCenter
-    #BUILD_COMMAND="./gradlew assemble && ./gradlew check && ./gradlew bintrayUpload -x check --info"
+    BUILD_COMMAND="./gradlew assemble && ./gradlew check && ./gradlew bintrayUpload -x check --info"
 fi
 bash -c "${BUILD_COMMAND}"

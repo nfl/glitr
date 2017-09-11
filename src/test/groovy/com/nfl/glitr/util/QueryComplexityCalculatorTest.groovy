a501abebc99610b5d95b6f5e34a6b93b63bc3332
Greg Baghdassarian <Greg.Baghdassarian@nfl.com>
2017-09-11 14:22:00 -0700

    Mutation addition and minor updates

--- src/test/groovy/com/nfl/glitr/util/QueryComplexityCalculatorTest.groovy
+++ src/test/groovy/com/nfl/glitr/util/QueryComplexityCalculatorTest.groovy
@@ -1,7 +1,6 @@
 package com.nfl.glitr.util
 
 import com.nfl.glitr.exception.GlitrException
-import com.nfl.glitr.util.complexity.QueryComplexityCalculator
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -11,7 +10,7 @@ class QueryComplexityCalculatorTest extends Specification {
     @Shared queryComplexityCalculator;
 
     def setup() {
-        queryComplexityCalculator = new QueryComplexityCalculator(200, 3, 10);
+        queryComplexityCalculator = new QueryComplexityCalculator(200, 3, 50,10);
     }
 
     @Unroll
@@ -49,6 +48,28 @@ class QueryComplexityCalculatorTest extends Specification {
         |        }
         |    }
         |}'''.stripMargin()            | "query with 69 chars" || 69
+
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackId
+        |            trackInfo {
+        |               id
+        |               name
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "query with 186 chars on the return mutation query" || 186
     }
 
     def "test query character score with exception [empty query]"() {
@@ -99,6 +120,26 @@ class QueryComplexityCalculatorTest extends Specification {
         |        }
         |    }
         |}'''.stripMargin()            | "query with 69 chars when max characters allowed is 200" || false
+
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackInfo {
+        |               id
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "query with 146 chars on the return mutation query when max allowed is 200" || false
     }
 
     @Unroll
@@ -149,6 +190,23 @@ class QueryComplexityCalculatorTest extends Specification {
         |    }
         |}'''.stripMargin()            | "query with depth 4" || 4
 
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackId
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "query with depth 2" || 2
     }
 
     def "test query depth score with exception [empty query]"() {
@@ -174,39 +232,6 @@ class QueryComplexityCalculatorTest extends Specification {
 
         where:
         query = '''{playLists{playListId}'''.stripMargin()
-
-    }
-
-    def "test query depth score with exception [no mutations allowed]"() {
-        when:
-        queryComplexityCalculator.depthScore(query)
-
-        then:
-        def exception = thrown(GlitrException)
-        exception.getMessage().equals("Cannot find query node 'OperationDefinition' or query is a 'MUTATION'")
-
-        where:
-        query = '''\
-        |mutation {
-        |      eventMtn(input: {
-        |        clientMutationId: "abc"
-        |        event: {
-        |          id: "abc"
-        |          propertyId: "32142020-4c41-5f53-5441-4c4c494f4e53"
-        |        }
-        |      }) {
-        |        event {
-        |          id
-        |          property {
-        |            id
-        |            franchises {
-        |               id
-        |               name
-        |            }
-        |          }
-        |        }
-        |      }
-        |    }'''.stripMargin()
 
     }
 
@@ -306,6 +331,28 @@ class QueryComplexityCalculatorTest extends Specification {
         |        }
         |    }
         |}'''.stripMargin()               | "query with depth 4 when max depth allowed is 3" || true
+
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackId
+        |            trackInfo {
+        |               id
+        |               name
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "mutation return query with depth 2 when max allowed is 3" || false
     }
 
     @Unroll
@@ -405,27 +452,178 @@ class QueryComplexityCalculatorTest extends Specification {
         |    }
         |}'''.stripMargin()               | "query with a score of 76" || 76
 
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackId
+        |            trackInfo {
+        |               id
+        |               name
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "mutation return query with a score of 60" || 60
+
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks(first:2) {
+        |            trackId
+        |            trackInfo(first:5) {
+        |               id
+        |               name
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "mutation return query with a score of 29" || 29
+
+
     }
 
     @Unroll
-    def "test check and retrieve query score, case: #name"() {
+    def "test query score limit, case: #name"() {
         expect:
-        int queryScore = queryComplexityCalculator.checkAndRetrieveQueryScore(query);
-        queryScore == expectedResult
+        boolean scoreLimitExceeded = queryComplexityCalculator.scoreLimitExceeded(query);
+        scoreLimitExceeded == expectedResult
 
         where:
-        query                          | name                       || expectedResult
+        query                              | name                 || expectedResult
         '''\
         |{
         | playListId
-        |}'''.stripMargin()            | "query with a score of 0"  || 0
+        |}'''.stripMargin()                | "query with score 0 when max score allowed is 50" || false
 
         '''\
         |{
         |    playLists {
         |        playListId
         |    }
-        |}'''.stripMargin()            | "query with a score of 10" || 10
+        |}'''.stripMargin()               | "query with score 10 when max score allowed is 50" || false
+
+        '''\
+        |{
+        |    playLists {
+        |        playListId
+        |        playListTitle
+        |        albums(first:3){
+        |           albumId
+        |        }
+        |    }
+        |}'''.stripMargin()               | "query with score 16 when max score allowed is 50" || false
+
+        '''\
+        |{
+        |    playLists {
+        |        playListId
+        |        playListTitle
+        |        albumsFirst(first:3){
+        |           albumId
+        |        }
+        |        albumsSecond(first:6){
+        |           albumId
+        |        }
+        |        albumsThird{
+        |           albumId
+        |        }
+        |    }
+        |}'''.stripMargin()               | "query with score 48 when max score allowed is 50" || false
+
+
+        '''\
+        |{
+        |    playLists {
+        |        playListId
+        |        playListTitle
+        |        albumsFirst(first:3){
+        |           albumId
+        |           trackPick1{
+        |              trackId
+        |           }
+        |           trackPick2{
+        |              trackid
+        |           }
+        |        }
+        |    }
+        |}'''.stripMargin()               | "query with score 76 when max score allowed is 50" || true
+
+        '''\
+        |{
+        |    playLists {
+        |        playListId
+        |        playListTitle
+        |        albumsFirst(first:3){
+        |           albumId
+        |           trackPick1{
+        |              trackId
+        |              trackInfo{
+        |                runTimeSecs
+        |                artist
+        |              }
+        |           }
+        |        }
+        |    }
+        |}'''.stripMargin()               | "query with score 86 when max score allowed is 50" || true
+
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackId
+        |            trackInfo {
+        |               id
+        |               name
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "query with score 60 when max score allowed is 50" || true
+    }
+
+    @Unroll
+    def "test validate query, case: #name"() {
+        expect:
+        queryComplexityCalculator.validate(query);
+
+        where:
+        query                          | name
+        '''\
+        |{
+        | playListId
+        |}'''.stripMargin()            | "query with a score of 0"
+
+        '''\
+        |{
+        |    playLists {
+        |        playListId
+        |    }
+        |}'''.stripMargin()            | "query with a score of 10"
 
         '''\
         |{
@@ -436,12 +634,30 @@ class QueryComplexityCalculatorTest extends Specification {
         |           albumId
         |        }
         |    }
-        |}'''.stripMargin()               | "query with a score of 16" || 16
+        |}'''.stripMargin()               | "query with a score of 16"
+
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackId
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "mutation return query with a score of 30"
     }
 
-    def "test check and retrieve query score with exception [characterLimitReached]"() {
+    def "test check valid query with exception [characterLimitReached]"() {
         when:
-        queryComplexityCalculator.checkAndRetrieveQueryScore(query)
+        queryComplexityCalculator.validate(query)
 
         then:
         def exception = thrown(GlitrException)
@@ -467,9 +683,9 @@ class QueryComplexityCalculatorTest extends Specification {
 
     }
 
-    def "test check and retrieve query score with exception [depthLimitReached]"() {
+    def "test check valid query with exception [depthLimitReached]"() {
         when:
-        queryComplexityCalculator.checkAndRetrieveQueryScore(query)
+        queryComplexityCalculator.validate(query)
 
         then:
         def exception = thrown(GlitrException)
@@ -493,4 +709,66 @@ class QueryComplexityCalculatorTest extends Specification {
 
     }
 
+    def "test check valid query with exception [scoreLimitReached]"() {
+        when:
+        queryComplexityCalculator.validate(query)
+
+        then:
+        def exception = thrown(GlitrException)
+        exception.getMessage().equals("query score has exceeded the maximum score level of 50.")
+
+        where:
+        query = '''\
+        |{
+        |    playLists {
+        |        albumsFirst(first:30){
+        |           albumId
+        |           trackPick1{
+        |              trackId
+        |           }
+        |        }
+        |    }
+        |}'''.stripMargin()
+
+    }
+
+    @Unroll
+    def "test check if query is mutation, case: #name"() {
+        expect:
+        boolean isMutation = queryComplexityCalculator.isMutation(query);
+        isMutation == expectedResult
+
+        where:
+        query                          | name                       || expectedResult
+        '''\
+        |{
+        | viewer{
+        |    test
+        |  }
+        |}'''.stripMargin()            | "query is not a mutation"  || false
+
+        '''\
+        |mutation {
+        |      playListMtn(input: {
+        |        clientId: "abc"
+        |        playList: {
+        |          playListId: "abc"
+        |          title: "MyPlaylist"
+        |        }
+        |      }) {
+        |        playList {
+        |          playListId
+        |          tracks {
+        |            trackId
+        |            trackInfo {
+        |               id
+        |               name
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }'''.stripMargin()       | "query is a mutation" || true
+
+
+    }
 }

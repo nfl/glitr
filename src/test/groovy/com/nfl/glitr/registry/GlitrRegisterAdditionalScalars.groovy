9cba25904f872c82cbf1940e1839c3436434f64e
arash.shokoufandeh <arash.shokoufandeh@nfl.com>
2017-11-27 12:12:10 -0800

    bumping to the latest release of graphql-java

--- src/test/groovy/com/nfl/glitr/registry/GlitrRegisterAdditionalScalars.groovy
+++ src/test/groovy/com/nfl/glitr/registry/GlitrRegisterAdditionalScalars.groovy
@@ -12,24 +12,24 @@ class GlitrRegisterAdditionalScalars extends Specification {
 
     def "Register custom scalars"() {
         when: "first discovery"
-        Glitr glitr = GlitrBuilder.newGlitr()
-                .withQueryRoot(new Root())
-                .addCustomScalar(CustomScalar.class, Scalars.GraphQLString)
-                .build()
-        def typeResolutionEnv = new TypeResolutionEnvironment(new Root(), null, null, null, null)
+            Glitr glitr = GlitrBuilder.newGlitr()
+                    .withQueryRoot(new Root())
+                    .addCustomScalar(CustomScalar.class, Scalars.GraphQLString)
+                    .build()
+            def typeResolutionEnv = new TypeResolutionEnvironment(new Root(), null, null, null, null, null)
         then: "make sure the scalar has been registered correctly as a GraphQLString"
-        glitr.typeRegistry.getType(typeResolutionEnv).getFieldDefinition("scalar").type.name == Scalars.GraphQLString.name
+            glitr.typeRegistry.getType(typeResolutionEnv).getFieldDefinition("scalar").type.name == Scalars.GraphQLString.name
     }
 
     def "Register twice the same custom scalar should fail"() {
         when: "first discovery"
-        Glitr glitr = GlitrBuilder.newGlitr()
-                .withQueryRoot(new Root())
-                .addCustomScalar(CustomScalar.class, Scalars.GraphQLString)
-                .addCustomScalar(CustomScalar.class, Scalars.GraphQLInt)
-                .build()
+            GlitrBuilder.newGlitr()
+                    .withQueryRoot(new Root())
+                    .addCustomScalar(CustomScalar.class, Scalars.GraphQLString)
+                    .addCustomScalar(CustomScalar.class, Scalars.GraphQLInt)
+                    .build()
         then: "make sure Glitr doesn't let the user attempt to register two scalars for the same java type."
-        def e = thrown(IllegalArgumentException)
-        e.getMessage().contains("You have previously registered the following Java type")
+            def e = thrown(IllegalArgumentException)
+            e.getMessage().contains("You have previously registered the following Java type")
     }
 }

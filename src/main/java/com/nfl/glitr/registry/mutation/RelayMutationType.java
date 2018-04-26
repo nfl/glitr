452a05407465ecfc6aa81ed61e321c9e47520771
arash.shokoufandeh <arash.shokoufandeh@nfl.com>
2018-04-26 13:51:51 -0700

    client mutation id should be optional

--- src/main/java/com/nfl/glitr/registry/mutation/RelayMutationType.java
+++ src/main/java/com/nfl/glitr/registry/mutation/RelayMutationType.java
@@ -1,14 +1,11 @@
 package com.nfl.glitr.registry.mutation;
 
-import com.nfl.glitr.annotation.GlitrNonNull;
-
 /**
  * Relay requires the notion of an Id passed by the client and is returned back to allow identification and
  * association of mutations
  */
 public abstract class RelayMutationType {
 
-    @GlitrNonNull
     private String clientMutationId;
 
 

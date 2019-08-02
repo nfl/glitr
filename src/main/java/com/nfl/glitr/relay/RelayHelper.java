b8c1d4974570070c455841598b1bdbfb23a36575
arashshokoufandeh <arash.shokoufandeh@omnigon.com>
2019-08-02 10:44:26 -0700

    back to jdk 11 and moving to using Base64 in java utils

--- src/main/java/com/nfl/glitr/relay/RelayHelper.java
+++ src/main/java/com/nfl/glitr/relay/RelayHelper.java
@@ -4,10 +4,10 @@ import com.nfl.glitr.registry.TypeRegistry;
 import graphql.relay.*;
 import graphql.schema.*;
 
-import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import static graphql.Assert.assertNotNull;
@@ -83,27 +83,27 @@ public class RelayHelper {
     }
 
     public static String createCursor(int offset) {
-        return Base64.toBase64(DUMMY_CURSOR_PREFIX + offset);
+        return Base64Helper.toBase64(DUMMY_CURSOR_PREFIX + offset);
     }
 
     public static int getOffsetFromCursor(String cursor, int defaultValue) {
         if (cursor == null) return defaultValue;
-        String string = Base64.fromBase64(cursor);
+        String string = Base64Helper.fromBase64(cursor);
         return Integer.parseInt(string.substring(DUMMY_CURSOR_PREFIX.length()));
     }
 
 
-    static public class Base64 {
+    static public class Base64Helper {
 
-        private Base64() {
+        private Base64Helper() {
         }
 
         public static String toBase64(String string) {
-            return DatatypeConverter.printBase64Binary(string.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(string.getBytes(StandardCharsets.UTF_8));
         }
 
         public static String fromBase64(String string) {
-            return new String(DatatypeConverter.parseBase64Binary(string), Charset.forName("UTF-8"));
+            return new String(Base64.getDecoder().decode(string), Charset.forName("UTF-8"));
         }
     }
 }

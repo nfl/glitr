479857d36a6d4781eae89c6e2eab59dece5e8bfa
omurzin <olexiy.murzin@omnigon.com>
2019-08-27 18:00:51 -0700

    fix for previousPageStartCursor calculation

--- src/test/groovy/com/nfl/glitr/relay/RelayHelperTest.groovy
+++ src/test/groovy/com/nfl/glitr/relay/RelayHelperTest.groovy
@@ -4,6 +4,7 @@ import com.nfl.glitr.GlitrBuilder
 import com.nfl.glitr.data.query.QueryType
 import com.nfl.glitr.util.SerializationUtil
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class RelayHelperTest extends Specification {
 
@@ -16,35 +17,38 @@ class RelayHelperTest extends Specification {
 
 
     @SuppressWarnings("GroovyPointlessBoolean")
+    @Unroll
     def "test build connection pagination"() {
         expect:
-        testPaging(toSkip, totalCount, hasNextPage, hasPrevPage, resultSize)
+            testPaging(offset, totalCount, hasNextPage, hasPrevPage, resultSize)
 
         where:
-        toSkip  ||  totalCount      ||  hasNextPage     ||  hasPrevPage ||  resultSize
-        0       ||  12              ||  true            ||  false       ||  10
-        1       ||  12              ||  false           ||  true        ||  10
-        2       ||  12              ||  false           ||  true        ||  10
-        7       ||  12              ||  false           ||  true        ||  5
-        69      ||  71              ||  false           ||  true        ||  2
-        70      ||  71              ||  false           ||  true        ||  1
-        1       ||  1               ||  false           ||  true        ||  0
-        50      ||  50              ||  false           ||  true        ||  0
-        11      ||  12              ||  false           ||  true        ||  1
-        0       ||  0               ||  false           ||  false       ||  0
-        0       ||  1               ||  false           ||  false       ||  1
-        1       ||  1               ||  false           ||  true        ||  0
+            offset || totalCount || hasNextPage || hasPrevPage || resultSize
+            0      || 12         || true        || false       || 10
+            1      || 12         || false       || true        || 10
+            2      || 12         || false       || true        || 10
+            7      || 12         || false       || true        || 5
+            69     || 71         || false       || true        || 2
+            70     || 71         || false       || true        || 1
+            1      || 1          || false       || true        || 0
+            50     || 50         || false       || true        || 0
+            11     || 12         || false       || true        || 1
+            0      || 0          || false       || false       || 0
+            0      || 1          || false       || false       || 1
+            1      || 1          || false       || true        || 0
+            25     || 50         || true        || true        || 10
     }
 
-    public void testPaging(def toSkip, def totalCount, def hasNext, def hasPrev, def resultSize) {
+    void testPaging(def offset, def totalCount, def hasNext, def hasPrev, def resultSize) {
         def items = []
-        for (def i = 0; i < 10; i++) {
-            if ((i + toSkip) < totalCount) {
-                items.add(i + toSkip)
+        def skipItem = 10
+        for (def i = 0; i < skipItem; i++) {
+            if ((i + offset) < totalCount) {
+                items.add(i + offset)
             }
         }
 
-        def connection = RelayHelper.buildConnection(items, toSkip, totalCount)
+        def connection = RelayHelper.buildConnection(items, offset, skipItem, totalCount)
         assert (connection.pageInfo.hasNextPage == hasNext)
         assert (connection.pageInfo.hasPreviousPage == hasPrev)
         assert (connection.edges.size() == resultSize)
@@ -53,11 +57,11 @@ class RelayHelperTest extends Specification {
         assert pageInfoWithTotal.total == totalCount
 
         if (resultSize > 0) {
-            assert (connection.pageInfo.startCursor?.value == RelayHelper.createCursor(toSkip))
-            assert (connection.pageInfo.endCursor?.value == RelayHelper.createCursor(toSkip + resultSize - 1))
+            assert (connection.pageInfo.startCursor?.value == RelayHelper.createCursor(offset))
+            assert (connection.pageInfo.endCursor?.value == RelayHelper.createCursor(offset + resultSize - 1))
 
-            if (toSkip - resultSize > 0) {
-                assert (pageInfoWithTotal.previousPageStartCursor?.value == RelayHelper.createCursor(toSkip - resultSize))
+            if (offset - skipItem > 0) {
+                assert (pageInfoWithTotal.previousPageStartCursor?.value == RelayHelper.createCursor(offset - skipItem))
             } else {
                 assert (pageInfoWithTotal.previousPageStartCursor?.value == RelayHelper.createCursor(0))
             }
